0bf50fa754f7919c8d8cb6e90e92b16d6f063dec
juanserna1711 <juan.serna@positex.co>
2026-09-09 11:31:03 -0500

    feat: implementar maquetación inicial con componentes de molinetes

---
+++ src/data/molinetesMock.js
@@ -0,0 +1,8 @@
+export const molinetesMock = [
+  { id: 1, nombre: "Molinete #1", rpm: 250, perimetro: 78},
+  { id: 2, nombre: "Molinete #2", rpm: 250, perimetro: 78},
+  { id: 3, nombre: "Molinete #3", rpm: 250, perimetro: 78},
+  { id: 4, nombre: "Molinete #4", rpm: 250, perimetro: 78},
+  { id: 5, nombre: "Molinete #5", rpm: 250, perimetro: 78},
+  { id: 6, nombre: "Molinete #6", rpm: 250, perimetro: 78}
+];
